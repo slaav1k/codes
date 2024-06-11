b860225fd7ca4eebaf57f89de15fec3ad1b572ba
slaav1k <slawa.121@yandex.ru>
2024-06-11 07:49:07 +0300

    leetcode

---
+++ python/leetcode/1122. Relative Sort Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        a = 0
+        for i in range(len(arr2)):
+            for j in range(len(arr1)):
+                if arr1[j] == arr2[i]:
+                    arr1[a], arr1[j] = arr1[j], arr1[a]
+                    a += 1
+        arr1[a:] = sorted(arr1[a:])
+        return arr1
