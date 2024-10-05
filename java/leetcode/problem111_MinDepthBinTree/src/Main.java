1ba5b515defa7668f11620496c3b140c19458be0
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-10-05 10:37:02 +0300

    Add files via upload

---
+++ java/leetcode/problem111_MinDepthBinTree/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(minDepth(root));
+    }
+
+    public static int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (left == 0 || right == 0)
+            return left + right + 1;
+        else
+            return Math.min(left, right) + 1;
+    }
+}
+
+
