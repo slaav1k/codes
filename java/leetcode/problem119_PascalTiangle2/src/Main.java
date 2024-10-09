d0edb1e6d1b227dabb6a892434d2f2d2a96a98a3
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-10-09 08:03:49 +0300

    Add files via upload

---
+++ java/leetcode/problem119_PascalTiangle2/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getRow(3));
+    }
+
+    public static List<Integer> getRow(int rowIndex) {
+        return generate(rowIndex + 1).get(rowIndex);
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) return result;
+
+        result.add(new ArrayList<>());
+        result.get(0).add(1);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> pref = result.get(i - 1);
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                row.add(pref.get(j - 1) + pref.get(j));
+            }
+            row.add(1);
+            result.add(row);
+        }
+        return result;
+    }
+}
