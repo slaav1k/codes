f5c1eb49a9186d486d3afb66eab60729cd1b5e39
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-10-13 10:12:36 +0300

    Add files via upload

---
+++ java/leetcode/problem145_BinTreePostTraversal/src/Main.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(postorderTraversal(root));
+    }
+
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.addFirst(node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return res;
+    }
+}
+
+
