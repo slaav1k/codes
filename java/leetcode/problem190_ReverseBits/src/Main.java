463a320d37caee27aae74e8fb4d0b054a3c53803
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-10-17 08:10:18 +0300

    Add files via upload

---
+++ java/leetcode/problem190_ReverseBits/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(reverseBits(00000010100101000001111010011100));
+    }
+
+    public static int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>>= 1;
+        }
+        return res;
+    }
+}
