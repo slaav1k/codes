ddf2893b79b4ae0b699100f807e0f05551187630
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-10-20 08:27:13 +0300

    Add files via upload

---
+++ java/leetcode/problem222_CountCompleteTreeNodes/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        System.out.println(countNodes(root));
+    }
+
+
+    public static int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left + right + 1;
+    }
+}
+
+
