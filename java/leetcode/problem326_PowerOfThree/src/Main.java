a5204382ea133717102d425a8cd052b088c8eb75
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-10-31 11:49:57 +0300

    Add files via upload

---
+++ java/leetcode/problem326_PowerOfThree/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(27));
+    }
+
+    public static boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
