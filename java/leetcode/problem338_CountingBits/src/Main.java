394247cb06652b31f95a1b1d0696cc99aa26b0c2
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-11-01 07:53:42 +0300

    Add files via upload

---
+++ java/leetcode/problem338_CountingBits/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countBits(5)));
+    }
+
+
+    public static int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            res[i] = res[i & (i - 1)] + 1;
+        }
+        return res;
+    }
+}
