77a27b795cce37ac3ddece04f2dd80e56a0d7d60
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-11-13 11:34:43 +0300

    Add files via upload

---
+++ java/leetcode/problem367_ValidPerfectSquare/src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPerfectSquare(5));
+    }
+
+    public static boolean isPerfectSquare(int num) {
+        if (num < 2) return true;
+
+        long left = 2, right = num / 2;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == num) return true;
+            else if (square < num) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
+    }
+}
