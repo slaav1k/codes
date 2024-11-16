d412ebdce2094ce5ff4cc3e09e4b7935bd927ba8
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-11-16 08:35:44 +0300

    Add files via upload

---
+++ java/leetcode/problem387_FirstUniqCharacterInAStr/src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("leetcode"));
+    }
+
+    public static int firstUniqChar(String s) {
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (arr[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
