78534d972bfc818e3dc1c8acc0b11195dacc19cc
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-11-19 10:26:40 +0300

    Add files via upload

---
+++ java/leetcode/problem392_IsSubsequence/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("acb", "ahbgdc"));
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+        if (s.length() > t.length()) return false;
+
+        int sIndex = 0;
+        int tIndex = 0;
+
+        while (sIndex < s.length() && tIndex < t.length()) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                sIndex++;
+            }
+            tIndex++;
+        }
+
+        return sIndex == s.length();
+    }
+
+}
