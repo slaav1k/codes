07ceaaa2f872e9d98179ac7b5123cbf34ff0c71e
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-11-23 10:04:24 +0300

    Add files via upload

---
+++ java/leetcode/problem412_FizzBuzz/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(15));
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}
