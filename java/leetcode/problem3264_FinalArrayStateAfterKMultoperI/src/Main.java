f58e037bfc043ad24de926bb70b2f4b87d709753
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-12-16 12:41:01 +0300

    Add files via upload

---
+++ java/leetcode/problem3264_FinalArrayStateAfterKMultoperI/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getFinalState(new int[]{2, 1, 3, 5, 6}, 5, 2)));
+    }
+
+    public static int[] getFinalState(int[] nums, int k, int multiplier) {
+        for (int i = 0; i < k; i++) {
+            int minIndex = 0;
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            nums[minIndex] *= multiplier;
+        }
+        return nums;
+    }
+}
