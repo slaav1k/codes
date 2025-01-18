a646e0ef716d6a5df0e7a4c967d0ed95ba1c9e86
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-01-18 14:00:29 +0300

    Add files via upload

---
+++ GO/leetcode/problem561/ArrayPartition.go
@@ -0,0 +1,20 @@
+package problem561
+
+import (
+	"fmt"
+	"sort"
+)
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	sum := 0
+	for i := 0; i < len(nums); i += 2 {
+		sum += nums[i]
+	}
+	return sum
+}
+
+func Runner() {
+	fmt.Println(arrayPairSum([]int{1, 4, 3, 2}))
+	fmt.Println(arrayPairSum([]int{6, 2, 6, 5, 1, 2}))
+}
