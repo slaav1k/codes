cbbfbae803747e40d16bf73cecacd268a7ba12fd
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-04-10 07:27:05 +0300

    Add files via upload

---
+++ GO/leetcode/problem590/NryTreePostorderTraversal4.go
@@ -0,0 +1,23 @@
+package problem590
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func postorder(root *Node) []int {
+	ret := []int{}
+
+	var visit func(node *Node)
+	visit = func(node *Node) {
+		if node == nil {
+			return
+		}
+		for _, child := range node.Children {
+			visit(child)
+		}
+		ret = append(ret, node.Val)
+	}
+	visit(root)
+	return ret
+}
