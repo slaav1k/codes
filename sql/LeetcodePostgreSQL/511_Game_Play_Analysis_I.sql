85e2fd59114dfbdd4ea6e3efe098229064af1ab9
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-05-02 13:05:41 +0300

    Add files via upload

---
+++ sql/LeetcodePostgreSQL/511_Game_Play_Analysis_I.sql
@@ -0,0 +1,19 @@
+-- Дано
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int)
+Truncate table Activity
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5')
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-05-02', '6')
+insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5')
+
+-- Задание
+-- Write a solution to find the first login date for each player.
+-- Return the result table in any order.
+-- The result format is in the following example.
+
+
+-- Решение
+select player_id, min(event_date) as first_login from activity
+group by player_id
+order by player_id
