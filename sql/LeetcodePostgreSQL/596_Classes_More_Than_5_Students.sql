ee7aa8ad8fdf9ff80ea03847ec43268f03cb9871
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-05-14 08:48:13 +0300

    Add files via upload

---
+++ sql/LeetcodePostgreSQL/596_Classes_More_Than_5_Students.sql
@@ -0,0 +1,22 @@
+-- Дано
+
+Create table If Not Exists Courses (student varchar(255), class varchar(255))
+Truncate table Courses
+insert into Courses (student, class) values ('A', 'Math');
+insert into Courses (student, class) values ('B', 'English');
+insert into Courses (student, class) values ('C', 'Math');
+insert into Courses (student, class) values ('D', 'Biology');
+insert into Courses (student, class) values ('E', 'Math');
+insert into Courses (student, class) values ('F', 'Computer');
+insert into Courses (student, class) values ('G', 'Math');
+insert into Courses (student, class) values ('H', 'Math');
+insert into Courses (student, class) values ('I', 'Math');
+
+-- Условие задачи
+-- Write a solution to find all the classes that have at least five students.
+-- Return the result table in any order.
+
+-- Решение
+select class from Courses
+group by(class)
+having count(student) >= 5
