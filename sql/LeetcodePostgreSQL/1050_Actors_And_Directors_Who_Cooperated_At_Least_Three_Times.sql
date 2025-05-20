55f03dfa38707aa01e69eb643d6d70414a6341aa
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-05-20 08:21:50 +0300

    Add files via upload

---
+++ sql/LeetcodePostgreSQL/1050_Actors_And_Directors_Who_Cooperated_At_Least_Three_Times.sql
@@ -0,0 +1,22 @@
+-- Дано
+Create table If Not Exists ActorDirector (actor_id int, director_id int, timestamp int)
+Truncate table ActorDirector
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '0');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '1');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '2');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '2', '3');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '2', '4');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('2', '1', '5');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('2', '1', '6');
+
+
+-- Условие задачи
+-- Write a solution to find all the pairs (actor_id, director_id)
+-- where the actor has cooperated with the director at least three times.
+-- Return the result table in any order.
+
+-- Решение
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(*) >= 3;
