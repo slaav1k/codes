3103b0d0d4d4c1b7b63d68e8be622461bfcf07f4
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-05-21 08:43:10 +0300

    Add files via upload

---
+++ sql/LeetcodePostgreSQL/1068_Product_Sales_Analysis_1.sql
@@ -0,0 +1,21 @@
+-- Дано
+select * from Product
+Create table If Not Exists Sales (sale_id int, product_id int, year int, quantity int, price int)
+Create table If Not Exists Product (product_id int, product_name varchar(10))
+Truncate table Sales
+insert into Sales (sale_id, product_id, year, quantity, price) values ('1', '100', '2008', '10', '5000');
+insert into Sales (sale_id, product_id, year, quantity, price) values ('2', '100', '2009', '12', '5000');
+insert into Sales (sale_id, product_id, year, quantity, price) values ('7', '200', '2011', '15', '9000');
+Truncate table Product
+insert into Product (product_id, product_name) values ('100', 'Nokia');
+insert into Product (product_id, product_name) values ('200', 'Apple');
+insert into Product (product_id, product_name) values ('300', 'Samsung');
+
+-- Условие задачи
+-- Write a solution to report the product_name, year,
+-- and price for each sale_id in the Sales table.
+-- Return the resulting table in any order.
+
+-- Решение
+select product_name, year, price from 
+product p join sales s on p.product_id = s.product_id
