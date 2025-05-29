a0bc05ec728d85e36e4f3a73dfd31261c43f87cf
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-05-29 19:16:39 +0300

    Add files via upload

---
+++ python/leetcode/problem3373/MaxTheNumOfTargetNodesAfterConnectingTree2.py
@@ -0,0 +1,48 @@
+from typing import List
+
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
+        n, m = len(edges1) + 1, len(edges2) + 1
+        adj1 = [[] for _ in range(n)]
+        adj2 = [[] for _ in range(m)]
+        mod2 = [False] * n
+
+        def build_adj(edges, adj):
+            for u, v in edges:
+                adj[u].append(v)
+                adj[v].append(u)
+
+        def dfs(i, parent, isEven, adj):
+            cnt = isEven
+            mod2[i] = isEven
+            for j in adj[i]:
+                if j == parent: continue
+                cnt += dfs(j, i, not isEven, adj)
+            return cnt
+
+        def dfs0(i, parent, isEven, adj):
+            cnt = isEven
+            for j in adj[i]:
+                if j == parent: continue
+                cnt += dfs0(j, i, not isEven, adj)
+            return cnt
+
+        build_adj(edges1, adj1)
+        build_adj(edges2, adj2)
+        y = dfs0(0, -1, True, adj2)
+        x = dfs(0, -1, True, adj1)
+
+        cnt2 = max(y, m - y)
+        cnt = ((n - x) + cnt2, x + cnt2)
+        return [cnt[b] for b in mod2]
+
+solution = Solution()
+edges1_1 = [[0,1], [0,2], [2,3], [2,4]]
+edges2_1 = [[0,1], [0,2], [0,3], [2,7], [1,4], [4,5], [4,6]]
+result1 = solution.maxTargetNodes(edges1_1, edges2_1)
+print("Пример 1:", result1)
+
+edges1_2 = [[0,1], [0,2], [0,3], [0,4]]
+edges2_2 = [[0,1], [1,2]]
+result2 = solution.maxTargetNodes(edges1_2, edges2_2)
+print("Пример 2:", result2)
