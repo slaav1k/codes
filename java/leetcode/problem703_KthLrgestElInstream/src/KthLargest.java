547e0f5e3056ee16270baa6ab2e8704653065697
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-05-30 08:57:08 +0300

    Add files via upload

---
+++ java/leetcode/problem703_KthLrgestElInstream/src/KthLargest.java
@@ -0,0 +1,24 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+class KthLargest {
+    private int k;
+    private PriorityQueue<Integer> pq;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<Integer>();
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > k) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
