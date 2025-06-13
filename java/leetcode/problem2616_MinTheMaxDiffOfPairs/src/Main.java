7a402d6d0fb2b6944a3421a176dbcf81da3f5da5
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-06-13 08:12:46 +0300

    Add files via upload

---
+++ java/leetcode/problem2616_MinTheMaxDiffOfPairs/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minimizeMax(new int[] {10, 1, 2, 7, 1, 3}, 2));
+        System.out.println(solution.minimizeMax(new int[] {4, 2, 1, 2}, 1));
+    }
+}
