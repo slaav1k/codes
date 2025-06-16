fc22d09aa6443e576026cef4a393b6c55e3ac0f5
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-06-16 19:12:17 +0300

    Add files via upload

---
+++ java/leetcode/problem2016_MaxDiffBetweenIncreasingEls/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maximumDifference(new int[] {7, 1, 5, 4}));
+        System.out.println(solution.maximumDifference(new int[] {9, 4, 3, 2}));
+        System.out.println(solution.maximumDifference(new int[] {1, 5, 2, 10}));
+    }
+}
