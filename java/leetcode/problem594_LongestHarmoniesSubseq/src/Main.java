bd58001234d6ba349d6aa7a03f61b84af52f8792
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-06-30 09:08:12 +0300

    Add files via upload

---
+++ java/leetcode/problem594_LongestHarmoniesSubseq/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().findLHS(new int[]{1,3,2,2,5,2,3,7}));
+        System.out.println(new Solution().findLHS(new int[]{1,2,3,4}));
+        System.out.println(new Solution().findLHS(new int[]{1,1,1,1}));
+    }
+}
---
+++ java/leetcode/problem594_LongestHarmoniesSubseq/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int maxLen = 0;
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                int len = map.get(key) + map.get(key + 1);
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+
+        return maxLen;
+    }
+}
