b5f8022e23231a061594037018d9e98a6909604c
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-07-30 08:02:31 +0300

    Add files via upload

---
+++ java/leetcode/problem2419_LongestSubArrWithMaxBitAND/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestSubarray(new int[]{1, 2, 3, 3, 2, 2}));
+        System.out.println(solution.longestSubarray(new int[]{1, 2, 3, 4}));
+
+    }
+}
