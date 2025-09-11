8350d18fd769b13ce1d71b62fd94b43d96262930
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-09-11 07:19:36 +0300

    Add files via upload

---
+++ java/leetcode/problem2785_SortVowelsInAStr/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public String sortVowels(String s) {
+        Set<Character> vowelsSet = Set.of('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+        List<Character> vowels = new ArrayList<>();
+
+        for (char c : s.toCharArray())
+            if (vowelsSet.contains(c))
+                vowels.add(c);
+
+        if (vowels.isEmpty())
+            return s;
+
+        Collections.sort(vowels);
+
+        StringBuilder result = new StringBuilder();
+        int index = 0;
+
+        for (char c : s.toCharArray()) {
+            if (vowelsSet.contains(c))
+                result.append(vowels.get(index++));
+            else
+                result.append(c);
+        }
+
+        return result.toString();
+    }
+}
