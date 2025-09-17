89c021239e06e750dd735854bf91b7a767eb3731
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-09-17 07:42:09 +0300

    Add files via upload

---
+++ java/leetcode/problem2353_DesignFoodRatingSystem/src/FoodRatings.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+class FoodRatings {
+    class Pair implements Comparable<Pair> {
+        String foodName;
+        int rating;
+
+        Pair(String name, int rate) {
+            foodName = name;
+            rating = rate;
+        }
+
+        public int compareTo(Pair obj) {
+            if (this.rating != obj.rating)
+                return obj.rating - this.rating;
+
+            return this.foodName.compareTo(obj.foodName);
+        }
+    }
+
+    Map<String, PriorityQueue<Pair>> cuisineToRatingsMap;
+    Map<String, Integer> foodNameToIndex;
+    Map<String, String> foodToCuisine;
+    Pair[] foodRatingsArray;
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        int n = foods.length;
+
+        cuisineToRatingsMap = new HashMap<>();
+        foodNameToIndex = new HashMap<>();
+        foodToCuisine = new HashMap<>();
+
+        foodRatingsArray = new Pair[n];
+
+        for (int i = 0; i < n; i++) {
+            foodRatingsArray[i] = new Pair(foods[i], ratings[i]);
+
+            foodNameToIndex.put(foods[i], i);
+            foodToCuisine.put(foods[i], cuisines[i]);
+
+            if (!cuisineToRatingsMap.containsKey(cuisines[i]))
+                cuisineToRatingsMap.put(cuisines[i], new PriorityQueue<>());
+
+            PriorityQueue<Pair> temp = cuisineToRatingsMap.get(cuisines[i]);
+            temp.offer(foodRatingsArray[i]);
+            cuisineToRatingsMap.put(cuisines[i], temp);
+        }
+    }
+
+    public void changeRating(String food, int newRating) {
+        int position = foodNameToIndex.get(food);
+
+        cuisineToRatingsMap.get(foodToCuisine.get(food)).remove(foodRatingsArray[position]);
+        foodRatingsArray[position].rating = newRating;
+        cuisineToRatingsMap.get(foodToCuisine.get(food)).offer(foodRatingsArray[position]);
+    }
+
+    public String highestRated(String cuisine) {
+        return Objects.requireNonNull(cuisineToRatingsMap.get(cuisine).peek()).foodName;
+    }
+}
---
+++ java/leetcode/problem2353_DesignFoodRatingSystem/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        FoodRatings foodRatings = new FoodRatings(
+                new String[] {"FoodRatings", "highestRated", "highestRated", "changeRating", "highestRated", "changeRating", "highestRated"},
+                new String[][]
+        )
+    }
+}
