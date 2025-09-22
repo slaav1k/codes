a70375fdc9569081c5d6982edac6c1530fb22f00
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-09-22 08:00:40 +0300

    Add files via upload

---
+++ java/leetcode/problem3005_CountElWirhMaxFreq/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.maxFrequencyElements(new int[]{1,2,2,3,1,4}));
+        System.out.println(sol.maxFrequencyElements(new int[]{1,2,3,4,5}));
+    }
+}
