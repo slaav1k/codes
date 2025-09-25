0b935b149f84799115fbbe464e48baa6b3518bde
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-09-25 07:13:01 +0300

    Add files via upload

---
+++ java/leetcode/problem120_Triangle/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> triangle = Arrays.asList(
+            Arrays.asList(2),
+            Arrays.asList(3, 4),
+            Arrays.asList(6, 5, 7),
+            Arrays.asList(4, 1, 8, 3)
+        );
+        System.out.println(solution.minimumTotal(triangle));
+    }
+}
