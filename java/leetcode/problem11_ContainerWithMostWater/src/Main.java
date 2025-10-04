c8dfd1eb11239401123feb472346eab5f0834741
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-10-04 08:30:24 +0300

    Add files via upload

---
+++ java/leetcode/problem11_ContainerWithMostWater/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+        System.out.println(solution.maxArea(new int[]{1, 1}));
+    }
+}
