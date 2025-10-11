29b25e90ecec010e6e1f5f48cf8fe503a1142707
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-10-11 09:08:05 +0300

    Add files via upload

---
+++ java/leetcode/problem3186_MaxTotalDamageWithSpellCasting/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.maximumTotalDamage(new int[] {1,1,3,4}));
+        System.out.println(s.maximumTotalDamage(new int[] {7,1,6,6}));
+    }
+}
