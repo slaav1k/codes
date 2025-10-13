28430ea2f3b02f3898e000a97cfe87e80526fb25
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-10-13 22:42:05 +0300

    Add files via upload

---
+++ java/leetcode/problem2273_FindResArrAfterRmAnagrams/src/main/java/rsreu/brigadaBubnova/Main.java
@@ -0,0 +1,11 @@
+package rsreu.brigadaBubnova;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.removeAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+        System.out.println(sol.removeAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+}
