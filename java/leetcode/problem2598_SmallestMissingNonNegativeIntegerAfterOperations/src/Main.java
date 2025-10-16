c1933334c24d121a493642fdcad8be3110be9a04
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-10-16 07:42:48 +0300

    Add files via upload

---
+++ java/leetcode/problem2598_SmallestMissingNonNegativeIntegerAfterOperations/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.findSmallestInteger(new int[]{1, -10, 7, 13, 6, 8}, 5));
+        System.out.println(s.findSmallestInteger(new int[]{1, -10, 7, 13, 6, 8}, 7));
+    }
+}
