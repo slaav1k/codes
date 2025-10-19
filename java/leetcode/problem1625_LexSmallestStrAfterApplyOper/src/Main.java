9c8753a93da3076861efd8654bf619cd7a9b7480
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-10-19 08:35:49 +0300

    Add files via upload

---
+++ java/leetcode/problem1625_LexSmallestStrAfterApplyOper/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.findLexSmallestString("5525", 9, 2));
+        System.out.println(sol.findLexSmallestString("74", 5, 1));
+        System.out.println(sol.findLexSmallestString("0011", 4, 2));
+    }
+}
