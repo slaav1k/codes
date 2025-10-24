cd343c160d3fa1c727b88adc3a0ee35c10a98a21
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-10-24 07:50:40 +0300

    Add files via upload

---
+++ java/leetcode/problem2048_NextGreaterNumBalancedNum/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.nextBeautifulNumber(1));
+        System.out.println(s.nextBeautifulNumber(1000));
+        System.out.println(s.nextBeautifulNumber(3000));
+    }
+}
