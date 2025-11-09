bf3b57a9d2a10a5995d79dc9e6f9fc070d68d221
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-11-09 09:21:47 +0300

    Add files via upload

---
+++ java/leetcode/problem2169_CountOperationsToObtainZero/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countOperations(2, 3));
+        System.out.println(solution.countOperations(10, 10));
+    }
+}
