e531a7dd5469e17a2302b11f2542051f80ce38dd
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-11-17 06:58:51 +0300

    Add files via upload

---
+++ java/leetcode/problem1437_CheckAll1atleastLengtK/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.kLengthApart(new int[] {1,0,0,0,1,0,0,1}, 2));
+        System.out.println(sol.kLengthApart(new int[] {1,0,0,1,0,1}, 2));
+    }
+}
