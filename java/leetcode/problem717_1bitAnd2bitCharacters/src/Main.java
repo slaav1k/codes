10b5751579e130c2279610545f68ea199d35100c
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-11-18 07:56:20 +0300

    Add files via upload

---
+++ java/leetcode/problem717_1bitAnd2bitCharacters/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isOneBitCharacter(new int[]{1, 0, 0}));
+        System.out.println(s.isOneBitCharacter(new int[]{1, 1, 1, 0}));
+    }
+}
