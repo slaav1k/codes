bd4291df626e4b1178ba439a7cc1af41c1c6581e
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-12-26 07:23:05 +0300

    Add files via upload

---
+++ java/leetcode/problem2483_MinPenaltyForAShop/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.bestClosingTime("YYNY"));
+        System.out.println(s.bestClosingTime("NNNNN"));
+        System.out.println(s.bestClosingTime("YYYY"));
+    }
+}
