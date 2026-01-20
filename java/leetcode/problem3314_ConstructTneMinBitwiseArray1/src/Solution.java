4abe9fa08be7667f0da03aa5d139105f0c1f349f
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-01-20 07:14:11 +0300

    Add files via upload

---
+++ java/leetcode/problem3314_ConstructTneMinBitwiseArray1/src/Solution.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+class Solution {
+
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = transform(nums.get(i));
+        }
+        return result;
+    }
+
+    private int transform(int val) {
+        if ((val & 3) == 1) {
+            return val - 1;
+        }
+
+        if ((val & 3) == 3) {
+            int shift = 0;
+            int temp = val >> 1;
+
+            while ((temp & 1) == 1) {
+                shift++;
+                temp >>= 1;
+            }
+            return val - (1 << shift);
+        }
+
+        return -1;
+    }
+}
