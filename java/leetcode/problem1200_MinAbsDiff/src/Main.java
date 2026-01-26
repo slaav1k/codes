dd023e78a587a790f71a800e9b0fe4cd21ac324f
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-01-26 07:59:26 +0300

    Add files via upload

---
+++ java/leetcode/problem1200_MinAbsDiff/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.minimumAbsDifference(
+                new int[] {4, 2, 1, 3}
+        ));
+
+        System.out.println(sol.minimumAbsDifference(
+                new int[] {1, 3, 6, 10, 15}
+        ));
+        System.out.println(sol.minimumAbsDifference(
+                new int[] {3, 8, -10, 23, 19, -4, -14, 27}
+        ));
+
+
+    }
+}
