207cb0046b601034df5b5afcb0d3ea4e30954707
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-02-01 12:41:58 +0300

    Add files via upload

---
+++ java/leetcode/problem3010_DivArrIntoSubArrWithMinCost/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minimumCost(new int[]{1, 2, 3, 12}));
+        System.out.println(solution.minimumCost(new int[]{5, 4, 3}));
+        System.out.println(solution.minimumCost(new int[]{10, 3, 1, 1}));
+    }
+}
