3d6f7450f7f987db662750b680e623b1c5a358fd
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-02-02 18:57:06 +0300

    Add files via upload

---
+++ java/leetcode/problem892_SurfaceArea3dShapes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.surfaceArea(
+                new int[][] {
+                        {1, 2},
+                        {3, 4}
+                }
+        ));
+    }
+}
