364e37f9ef3df1f24ab2f8ddb7cdd37c9377877a
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-02-03 20:51:50 +0300

    Add files via upload

---
+++ java/leetcode/problem3637_TrionicArr1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isTrionic(new int[]{1,3,5,4,2,6}));
+        System.out.println(s.isTrionic(new int[]{2,1,3}));
+    }
+}
