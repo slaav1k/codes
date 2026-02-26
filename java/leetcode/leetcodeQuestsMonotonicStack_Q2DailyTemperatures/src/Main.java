126ceb61084f759cb84b221c7b80b1dda169a9dc
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-02-26 07:55:55 +0300

    Add files via upload

---
+++ java/leetcode/leetcodeQuestsMonotonicStack_Q2DailyTemperatures/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.dailyTemperatures(
+                new int[] {73,74,75,71,69,72,76,73}
+        )));
+    }
+}
