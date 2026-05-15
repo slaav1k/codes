965ec1d737b4df61d576a8cf2d6407396aec8632
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-05-15 07:19:07 +0300

    Add files via upload

---
+++ java/leetcode/problem0153_FindMinimumInRotatedSortedArray/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.findMin(new int[]{3, 4, 5, 1, 2}));
+        System.out.println(s.findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
+        System.out.println(s.findMin(new int[]{11, 13, 15, 17}));
+    }
+}
