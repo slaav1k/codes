8d9423d796e19c1fcf8eb35c051611d6ffa51f23
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-05-16 08:43:55 +0300

    Add files via upload

---
+++ java/leetcode/problem0154_FindMinimumInRoratedSortedArray2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.findMin(new int[]{1, 3, 5}));
+        System.out.println(s.findMin(new int[]{2, 2, 2, 0, 1}));
+    }
+}
---
+++ java/leetcode/problem0154_FindMinimumInRoratedSortedArray2/src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        int mid;
+        while (l < r) {
+            mid = (l + r) / 2;
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else if (nums[mid] < nums[r]) {
+                r = mid;
+            } else {
+                r--;
+            }
+        }
+        return nums[r];
+    }
+}
