85f3b22d4c071feb02f2c71503f37d02bf84700b
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-06-18 08:07:48 +0300

    Add files via upload

---
+++ java/leetcode/problem1344_AngleBetweenHandsOfAClock/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.angleClock(12, 30));
+        System.out.println(s.angleClock(3, 30));
+        System.out.println(s.angleClock(3, 15));
+    }
+}
