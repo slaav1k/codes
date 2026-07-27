765fb2e396edda4efa34a4cdacd566633152c1e4
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-07-27 13:09:39 +0300

    Add files via upload

---
+++ java/leetcode/problem1464_MaxProductofTwoElementsArray/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.maxProduct(new int[]{3, 4, 5, 2}));
+        System.out.println(s.maxProduct(new int[]{1, 5, 4, 5}));
+        System.out.println(s.maxProduct(new int[]{3, 7}));
+
+    }
+
+}
