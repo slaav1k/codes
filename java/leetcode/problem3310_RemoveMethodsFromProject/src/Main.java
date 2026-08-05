037c9c1f4288e2ef12820efd75b31b2d26048e41
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-08-05 09:03:59 +0300

    Add files via upload

---
+++ java/leetcode/problem3310_RemoveMethodsFromProject/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.remainingMethods(4, 1, new int[][] {
+                new int[] {1, 2},
+                new int[] {0, 1},
+                new int[] {3, 2}
+        }));
+    }
+}
