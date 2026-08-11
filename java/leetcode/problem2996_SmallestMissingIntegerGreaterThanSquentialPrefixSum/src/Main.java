fd81307e734781d6d58816a3c70edb30df063772
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-08-11 08:01:21 +0300

    Add files via upload

---
+++ java/leetcode/problem2996_SmallestMissingIntegerGreaterThanSquentialPrefixSum/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.missingInteger(new int[] {
+                1, 2, 3, 2, 5
+        }));
+        System.out.println(s.missingInteger(new int[] {
+                3, 4, 5, 1, 12, 14, 13
+        }));
+    }
+}
