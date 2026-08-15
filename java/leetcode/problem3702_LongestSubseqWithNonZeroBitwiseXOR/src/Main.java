203a2c7766617fdde48dad6b12dc97867c962b5d
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2026-08-15 09:13:03 +0300

    Add files via upload

---
+++ java/leetcode/problem3702_LongestSubseqWithNonZeroBitwiseXOR/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.longestSubsequence(new int[]{1, 2, 3}));
+        System.out.println(s.longestSubsequence(new int[]{2, 3, 4}));
+
+    }
+}
